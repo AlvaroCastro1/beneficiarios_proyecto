5c7c6fb508653e21012ae2f3fb6c2cbba068dd5d
AlvaroCastro1 <jesus.27castrop@gmail.com>
2024-03-20 19:30:47 -0600

    javadoc

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.beneficiarios"},{"l":"com.mycompany.beneficiarios.CRUD"},{"l":"com.mycompany.beneficiarios.reporte"}];updateSearchResults();
