6a1ef28c0d31fb3cdf3a195a53acb9bd69c9be6f
AlvaroCastro1 <jesus.27castrop@gmail.com>
2024-03-28 10:57:03 -0600

    image

---
+++ src/com/mycompany/beneficiarios/NewMain.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.beneficiarios;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.net.URL;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s =System.getProperty("user.home");
+        System.out.println(s);
+    }
+        
+}
